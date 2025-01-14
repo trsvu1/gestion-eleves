c94da0eefef7de0ed548ef7eb4cd13295d7ce622
Son Wu <161489776+trsvu1@users.noreply.github.com>
2025-01-14 10:03:56 +0100

    Update controleur.class.php

--- controleur/controleur.class.php
+++ controleur/controleur.class.php
@@ -4,26 +4,19 @@
 		private $unModele ; 
 
 		public function   __construct(){
-			
 			$this->unModele = new Modele (); 
 		}
 		/****************** Gestion des eleves *****************/
 		public function insertEleve($tab){
-			 
-
-		
 			$this->unModele->insertEleve($tab);
 		}
 
 		public function selectAllEleves ($filtre){
 			$lesEleves = $this->unModele->selectAllEleves($filtre); 
-
-			
-
 			return $lesEleves; 
 		}
+		
 		public function deleteEleve($id_eleve){
-			 
 			$this->unModele->deleteEleve($id_eleve);
 		}
 
